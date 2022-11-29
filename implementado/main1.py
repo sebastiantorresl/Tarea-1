88332bc4848076457a5e74123d91bf9435df5292
sebastiantorresl <sebastian_torres_lopez@hotmail.com>
2022-11-29 05:07:17 -0600

    archivos de la primera corrida con las implementaciones

--- implementado/main1.py
+++ implementado/main1.py
@@ -4,7 +4,7 @@ import pickle
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
-net = networka.Network([784, 20, 10], cost=networka.CrossEntropyCost)
+net = network.Network([784, 20, 10], cost=network.CrossEntropyCost)
 net.large_weight_initializer()
 net.SGD(training_data, 30, 20, 0.25, evaluation_data=test_data, monitor_evaluation_accuracy=True)
 archivo = open("red_prueba.pkl", 'wb')
